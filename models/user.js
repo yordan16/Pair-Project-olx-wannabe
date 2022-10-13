f6cb51f1dd37965e186fa4f22ccd7e984eb1046c
yordangurinoputro <gurinoputroyordan@gmail.com>
2022-10-13 21:11:40 +0700

    fix login logout bisa banget

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@
 const {
   Model
 } = require('sequelize');
+const bcrypt = require("bcryptjs")
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -17,6 +18,13 @@ module.exports = (sequelize, DataTypes) => {
     email: DataTypes.STRING,
     password: DataTypes.STRING
   }, {
+    hooks: {
+      beforeCreate(instance, options) {
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(instance.password, salt)
+        instance.password = hash
+      }
+    },
     sequelize,
     modelName: 'User',
   });
